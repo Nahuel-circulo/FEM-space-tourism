672c8615847437cfe2883234f9523118472e10b9
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-04-28 21:18:50 -0300

    fix desktop destination styles

--- src/components/Destinations.tsx
+++ src/components/Destinations.tsx
@@ -52,7 +52,7 @@ const Destinations = () => {
         id="destination"
         className=" h-auto min-h-screen bg-destination-mobile bg-cover  px-8 py-6 font-barlow font-normal md:bg-destination-tablet md:px-16 lg:bg-destination-desktop grid"
       >
-        <div className="mx-auto grid w-full max-w-md place-items-center lg:gap-10  pt-12 md:max-w-2xl lg:max-w-6xl lg:grid-cols-2 lg:auto-rows-auto lg:place-items-center ">
+        <div className="mx-auto grid w-full max-w-md place-items-center lg:gap-10  pt-12 md:max-w-2xl lg:max-w-6xl lg:grid-cols-2 lg:auto-rows-auto  ">
           <h2 className=" py-4 text-xl lg:text-3xl uppercase text-white md:justify-self-start md:py-8 lg:col-start-1 lg:self-end">
             <span className="pr-4 font-bold text-gray-500 ">01</span>
             Pick yourdestination
@@ -62,9 +62,9 @@ const Destinations = () => {
             src={currentDestination?.images.webp}
             alt="moon"
           />
-          <div className=" mx-auto w-full   self-end  px-4  text-center  font-barlow text-base  text-white-text md:text-lg lg:col-start-2 lg:row-start-2 lg:max-w-2xl lg:self-center">
+          <div className=" mx-auto w-full   self-end  px-4  text-center  font-barlow text-base  text-white-text md:text-lg lg:col-start-2 lg:row-start-2 lg:max-w-2xl lg:self-center  lg:text-start">
             {
-              <div className="flex  justify-center gap-4 whitespace-nowrap uppercase">
+              <div className="flex  justify-center gap-4 lg:gap-8 whitespace-nowrap uppercase lg:justify-start">
                 {destinations.map((item: IDestination) => (
                   <button
                     onClick={() => setCurrentDestination(item)}
@@ -73,7 +73,7 @@ const Destinations = () => {
                       currentDestination?.name === item.name
                         ? " border-white text-white"
                         : "border-transparent text-gray-300"
-                    }  inline-flex h-10 items-center whitespace-nowrap   border-b-2     text-sm  uppercase  focus:outline-none sm:text-base`}
+                    }  inline-flex h-10 items-center whitespace-nowrap   border-b-2     text-sm lg:text-lg  uppercase  focus:outline-none sm:text-base`}
                   >
                     {item.name}
                   </button>
@@ -83,17 +83,17 @@ const Destinations = () => {
             <h1 className="my-4 font-bellefair text-6xl uppercase text-white md:text-8xl lg:text-9xl">
               {currentDestination?.name}
             </h1>
-            <p className="mx-auto py-2 leading-7 md:max-w-sm md:text-base lg:max-w-md lg:text-justify lg:text-lg lg:leading-9">
+            <p className="mx-auto py-2 leading-7 md:max-w-sm md:text-base lg:max-w-md lg:text-justify lg:text-lg lg:leading-9 lg:m-0">
               {currentDestination?.description}
             </p>
-            <div className=" mt-8 grid w-full place-items-center gap-8 border-t-2  border-solid border-gray-500 p-8 font-bellefair uppercase md:grid-cols-2 lg:col-start-2 lg:row-start-3">
-              <div className="w-full text-center">
+            <div className=" mt-8 grid w-full place-items-center gap-8 border-t-2 lg:py-8  lg:max-w-md lg:px-0  border-solid border-gray-500 p-8 font-bellefair uppercase md:grid-cols-2 lg:col-start-2 lg:row-start-3">
+              <div className="w-full text-center lg:text-start">
                 <p className="text-base text-white-text ">AVG. DISTANCE</p>
                 <h3 className="text-3xl text-white">
                   {currentDestination?.distance}
                 </h3>
               </div>
-              <div className="w-full text-center">
+              <div className="w-full text-center lg:text-start">
                 <p className="text-base text-white-text">Est. travel time</p>
                 <h3 className="text-3xl text-white">
                   {currentDestination?.travel}
